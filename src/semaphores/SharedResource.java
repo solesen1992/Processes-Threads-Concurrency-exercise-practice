c4680589094e58f35a847c5a9ce6e3a76cec9fd8
solesen1992 <saramundal@live.dk>
2024-08-19 00:01:01 +0200

    Create SharedResource

---
+++ src/semaphores/SharedResource.java
@@ -0,0 +1,9 @@
+package semaphores;
+
+/**
+ * The Shared class has a static integer count that is shared among the threads and initially set to 0.
+ * */
+
+public class SharedResource {
+	static int count = 0;
+}
